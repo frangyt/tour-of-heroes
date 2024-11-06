2313e7b5c69e6583ae6c49023eae5ae401972841
sz-leandro-cunha <leandro.frangit@gmail.com>
2024-11-06 14:18:27 -0300

    2. Display a list

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { 
+  NgFor, 
+  NgIf,
+  UpperCasePipe 
+} from '@angular/common';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule, 
+    NgFor, 
+    NgIf, 
+    UpperCasePipe
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
