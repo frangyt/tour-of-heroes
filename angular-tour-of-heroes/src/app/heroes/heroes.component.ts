5bb9e178aa76795726adf457ed841d9f6f028f4a
sz-leandro-cunha <leandro.frangit@gmail.com>
2024-11-06 14:40:04 -0300

    3. Create a feature component

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { 
   NgFor, 
-  NgIf,
   UpperCasePipe 
 } from '@angular/common';
 import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
     FormsModule, 
-    NgFor, 
-    NgIf, 
-    UpperCasePipe
+    NgFor,  
+    UpperCasePipe,
+    HeroDetailComponent
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
@@ -23,7 +23,7 @@ import { Hero } from '../hero';
 export class HeroesComponent {
   heroes = HEROES;
   selectedHero?: Hero;
-  
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
